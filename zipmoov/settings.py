4b9f51782eb422ae0a9208c44fd03c0d735afec9
atul <jain.atul43@gmial.com>
2017-11-14 06:53:15 +0000

    server changes

--- zipmoov/settings.py
+++ zipmoov/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/client/static/'
-STATIC_ROOT = '/client/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, "static")
 # STATIC_URL = '/clients/static/'
 
 STATICFILES_DIRS = (
